56491813d5b1bfb8ff8a89d6983c0ce28d42680f
Héctor de la Rosa Prado <hoftherose@gmail.com>
2026-09-04 16:27:44 -0600

    Tabbing fixed

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,14 +2,14 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: false },
-  modules: ['shadcn-nuxt'],
-  css: ['~/assets/css/tailwind.css'],
-  vite: {
-    plugins: [tailwindcss()]
-  },
-  shadcn: {
-    prefix: ''
-  }
+    compatibilityDate: '2025-07-15',
+    devtools: { enabled: false },
+    modules: ['shadcn-nuxt'],
+    css: ['~/assets/css/tailwind.css'],
+    vite: {
+        plugins: [tailwindcss()]
+    },
+    shadcn: {
+        prefix: ''
+    }
 })
--- server/api/auth/[...all].ts
+++ server/api/auth/[...all].ts
@@ -2,11 +2,11 @@ import { auth } from "~~/lib/auth";
 import { toWebRequest } from "h3";
 
 export default defineEventHandler(async (event) => {
-  try {
-    const webRequest = toWebRequest(event);
-    const res = await auth.handler(webRequest);
-    return new Response(res.body, res);
-  } catch (err: any) {
-    throw createError({ statusCode: 500, message: String(err?.message || err), stack: err?.stack });
-  }
+    try {
+        const webRequest = toWebRequest(event);
+        const res = await auth.handler(webRequest);
+        return new Response(res.body, res);
+    } catch (err: any) {
+        throw createError({ statusCode: 500, message: String(err?.message || err), stack: err?.stack });
+    }
 });
