e6ff710d354493721dc0d8699ff414a9474ca56f
Héctor de la Rosa Prado <hoftherose@gmail.com>
2026-09-05 10:40:55 -0600

    No div in main app

--- app/app.vue
+++ app/app.vue
@@ -2,8 +2,6 @@
 </script>
 
 <template>
-    <div class="flex min-h-screen w-full flex-col items-center justify-center gap-6 p-4">
-        <NuxtRouteAnnouncer />
-        <NuxtPage />
-    </div>
+    <NuxtRouteAnnouncer />
+    <NuxtPage />
 </template>
