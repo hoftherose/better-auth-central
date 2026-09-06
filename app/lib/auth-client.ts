4d1d74d8c4c1b6b131d09561f1eedbc6659895cc
Héctor de la Rosa Prado <hoftherose@gmail.com>
2026-09-05 19:21:56 -0600

    Add baseURL

--- app/lib/auth-client.ts
+++ app/lib/auth-client.ts
@@ -2,6 +2,7 @@ import { createAuthClient } from "better-auth/vue";
 import { adminClient } from "better-auth/client/plugins";
 
 export const authClient = createAuthClient({
+    baseURL: "http://localhost:3000/",
     plugins: [
         adminClient()
     ]
--- lib/auth.ts
+++ lib/auth.ts
@@ -3,6 +3,8 @@ import { admin, openAPI } from "better-auth/plugins";
 import Database from "better-sqlite3";
 
 export const auth = betterAuth({
+    baseURL: "http://localhost:3000",
+    trustedOrigins: ["http://localhost:3000"],
     database: new Database("./sqlite.db"),
     emailAndPassword: {
         enabled: true,
