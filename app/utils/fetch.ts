fceee62409dbcbdc0aeb94e9c87b249953025e3e
Héctor de la Rosa Prado <hoftherose@gmail.com>
2026-09-07 20:09:45 -0600

    useFetch fix works in ssl (should only be used for internal communication like this)

--- app/utils/fetch.ts
+++ app/utils/fetch.ts
@@ -2,7 +2,7 @@ import { useFetch } from '#app'
 
 export default ((url: string, opts?: any) => {
     try {
-        if (url.startsWith('http')) url = new URL(url).pathname
+        if (url.startsWith('http') || url.startsWith('https')) url = new URL(url).pathname
     } catch {}
     return useFetch(url, opts)
 }) as any
